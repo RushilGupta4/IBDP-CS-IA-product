cfdf3c6da104c7f8027d48ceb5d8bade4435bf3f
Ruzil357 <rushilgupta4@gmail.com>
2023-02-14 10:09:39 +0530

    Fixed postgres data saving and added dashboard icons

--- client/src/data/DashboardData.js
+++ client/src/data/DashboardData.js
@@ -1,7 +1,7 @@
 export const CommonDashboardItems = [
     {
         name: "Hall Of Fame",
-        img: "/favicon.ico",
+        img: "/images/hall-of-fame.png",
         to: "/hall-of-fame",
     },
 ]
@@ -10,27 +10,27 @@ export const CommonDashboardItems = [
 export const AdminDashboardItems = [
     {
         name: "See Attendance",
-        img: "/favicon.ico",
+        img: "/images/see-attendance.png",
         to: "/admin/see-attendance",
     },
     {
         name: "Take Attendance",
-        img: "/favicon.ico",
+        img: "/images/take-attendance.png",
         to: "/admin/take-attendance",
     },
     {
         name: "Choose Employee",
-        img: "/favicon.ico",
+        img: "/images/employee-of-the-month.png",
         to: "/admin/employee-of-the-month",
     },
     {
         name: "Pending Applications",
-        img: "/favicon.ico",
+        img: "/images/apply-for-leave.png",
         to: "/admin/pending-applications",
     },
     {
         name: "Past Applications",
-        img: "/favicon.ico",
+        img: "/images/past-applications.png",
         to: "/admin/past-applications",
     },
 ]
@@ -39,17 +39,17 @@ export const AdminDashboardItems = [
 export const EmployeeDashboardItems = [
     {
         name: "See Attendance",
-        img: "/favicon.ico",
+        img: "/images/see-attendance.png",
         to: "/employee/see-attendance",
     },
     {
         name: "Apply For Leave",
-        img: "/favicon.ico",
+        img: "/images/apply-for-leave.png",
         to: "/employee/apply-for-leave",
     },
     {
         name: "Pending/Past Applications",
-        img: "/favicon.ico",
+        img: "/images/past-applications.png",
         to: "/employee/past-applications",
     },
 ]
